5fb99359cef1f38853ac9e703a11d3b630698134
caiogodoyy <caio.godoy.vg@gmail.com>
2023-11-25 10:10:47 -0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+(function () {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBwKEsfVbBY_VGva7QfXGD-MxTzqrISi-c",
+        authDomain: "alarm-esp32.firebaseapp.com",
+        databaseURL: "https://alarm-esp32-default-rtdb.firebaseio.com",
+        projectId: "alarm-esp32",
+        storageBucket: "alarm-esp32.appspot.com",
+        messagingSenderId: "309892326490",
+        appId: "1:309892326490:web:f362067ab10f8bcdca6463",
+        measurementId: "G-75BVZYF4EJ"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})();
